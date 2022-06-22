be5944aae3aa73d0be30246d4db2dcc228a13048
Akila-Piumal <akilapiumal16@gmail.com>
2022-06-22 22:05:33 +0530

    ManageStudentForm Added

--- src/lk/ijse/Hostel_Management_System/controller/DashBoardFormController.java
+++ src/lk/ijse/Hostel_Management_System/controller/DashBoardFormController.java
@@ -1,4 +1,40 @@
 package lk.ijse.Hostel_Management_System.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class DashBoardFormController {
+    public AnchorPane dashBoardFormContext;
+    public AnchorPane childFormContext;
+
+    public void backToHomeOnAction(MouseEvent mouseEvent) {
+
+    }
+
+    public void manageStudentsFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("manageStudents");
+    }
+
+    public void manageRoomsFormOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void reservationDetailsFormOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void makeReservationFormOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void setUi(String URI) throws IOException {
+        childFormContext.getChildren().clear();
+        Parent parent = FXMLLoader.load(getClass().getResource("../view/"+ URI +"Form.fxml"));
+        childFormContext.getChildren().add(parent);
+    }
 }
---
+++ src/lk/ijse/Hostel_Management_System/controller/ManageStudentsFormController.java
@@ -0,0 +1,26 @@
+package lk.ijse.Hostel_Management_System.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
+public class ManageStudentsFormController {
+    public JFXTextField txtStudentId;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContactNo;
+    public JFXTextField txtDob;
+    public JFXComboBox cmbGender;
+    public JFXButton btnAdd;
+    public TableView tblStudentDetails;
+
+    public void initialize(){
+
+    }
+
+    public void btnAddStudentOnAction(ActionEvent actionEvent) {
+
+    }
+}
