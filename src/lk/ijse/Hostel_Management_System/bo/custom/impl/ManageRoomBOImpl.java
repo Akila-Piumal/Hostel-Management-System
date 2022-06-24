d598a677fdc38966862a7e7017f59a3cdc1402bc
Akila-Piumal <akilapiumal16@gmail.com>
2022-06-24 13:26:06 +0530

    getAllRooms method refactored in ManageRoomBOImpl

--- src/lk/ijse/Hostel_Management_System/bo/custom/impl/ManageRoomBOImpl.java
+++ src/lk/ijse/Hostel_Management_System/bo/custom/impl/ManageRoomBOImpl.java
@@ -1,13 +1,26 @@
 package lk.ijse.Hostel_Management_System.bo.custom.impl;
 
 import lk.ijse.Hostel_Management_System.bo.custom.ManageRoomBO;
+import lk.ijse.Hostel_Management_System.dao.DAOFactory;
+import lk.ijse.Hostel_Management_System.dao.SuperDAO;
+import lk.ijse.Hostel_Management_System.dao.custom.RoomDAO;
 import lk.ijse.Hostel_Management_System.dto.RoomDTO;
+import lk.ijse.Hostel_Management_System.entity.Room;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ManageRoomBOImpl implements ManageRoomBO {
+
+    RoomDAO roomDAO = (RoomDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ROOM);
+
     @Override
     public List<RoomDTO> getAllRooms() {
-        return null;
+        List<Room> all = roomDAO.getAll();
+        List<RoomDTO> allRooms=new ArrayList<>();
+        for (Room room : all) {
+            allRooms.add(new RoomDTO(room.getRoomTypeId(),room.getType(),room.getKeyMoney(),room.getQty()));
+        }
+        return allRooms;
     }
 }
