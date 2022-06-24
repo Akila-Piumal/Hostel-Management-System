19e84f837eabca4b50946208872a45af6f85482b
Akila-Piumal <akilapiumal16@gmail.com>
2022-06-24 15:00:16 +0530

    set data to the textFields

--- src/lk/ijse/Hostel_Management_System/controller/ManageRomsFormController.java
+++ src/lk/ijse/Hostel_Management_System/controller/ManageRomsFormController.java
@@ -5,11 +5,13 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 import lk.ijse.Hostel_Management_System.bo.BOFactory;
 import lk.ijse.Hostel_Management_System.bo.SuperBO;
 import lk.ijse.Hostel_Management_System.bo.custom.ManageRoomBO;
 import lk.ijse.Hostel_Management_System.dto.RoomDTO;
 import lk.ijse.Hostel_Management_System.entity.Room;
+import lk.ijse.Hostel_Management_System.util.AnimationUtil;
 import lk.ijse.Hostel_Management_System.view.tdm.RoomTM;
 
 import java.util.List;
@@ -23,14 +25,24 @@ public class ManageRomsFormController {
     public JFXButton btnSave;
     public JFXButton btnDelete;
     public TableView<RoomTM> tblRoomDetails;
+    public AnchorPane manageRoomsFormContext;
 
     ManageRoomBO manageRoomBO = (ManageRoomBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.MANAGEROOMS);
 
     public void initialize(){
+        AnimationUtil.windowAnimation(manageRoomsFormContext);
+
         List<RoomDTO> allRooms = manageRoomBO.getAllRooms();
         for (RoomDTO roomDTO : allRooms) {
             cmbRoomTypeID.getItems().add(roomDTO);
         }
+
+        cmbRoomTypeID.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            txtType.setText(newValue.getType());
+            txtKeyMoney.setText(newValue.getKeyMoney());
+            txtQty.setText(String.valueOf(newValue.getQty()));
+        });
+
     }
 
     public void btnNewRoomOnAction(ActionEvent actionEvent) {
